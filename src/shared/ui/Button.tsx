69a699bc7893d7c70667b642d97256f715f7efb4
d5ng <dong5ffice@gmail.com>
2025-06-01 01:13:12 +0900

    feat(button): update props

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -1,6 +1,7 @@
 import type { ButtonHTMLAttributes } from "react"
-import { Slot, Slottable } from "../lib"
 import { type RecipeVariants } from "@vanilla-extract/recipes"
+import clsx from "clsx"
+import { Slot, Slottable } from "../lib"
 import { button } from "./Button.css"
 
 export type ButtonVariants = NonNullable<RecipeVariants<typeof button>>
@@ -9,10 +10,10 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants 
   asChild?: boolean
 }
 
-export default function Button({ children, asChild, variant = "primary", ...restProps }: Props) {
+export default function Button({ children, asChild, className, variant = "primary", ...restProps }: Props) {
   const Element = asChild ? Slot : "button"
   return (
-    <Element className={button({ variant })} {...restProps}>
+    <Element className={clsx(button({ variant }), className)} {...restProps}>
       <Slottable>{children}</Slottable>
     </Element>
   )
