5f5d30e09a3a6edf84fdad999def1102dbf22559
d5ng <dong5ffice@gmail.com>
2025-06-05 12:56:30 +0900

    delete: remove DayCell component and its associated styles and constants

--- src/shared/ui/Calendar/dayCell/DayCell.type.ts
+++
@@ -1,6 +0,0 @@
-import type { RecipeVariants } from "@vanilla-extract/recipes"
-import type { dayCell } from "./DayCell.css"
-
-export type DayCellVariants = NonNullable<RecipeVariants<typeof dayCell>>
-
-export type DayCellVariantType = NonNullable<DayCellVariants["variant"]>
