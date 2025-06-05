6e82a5f3c966f5dd0acf49bad0cda4e6446a25e6
d5ng <dong5ffice@gmail.com>
2025-06-05 10:36:24 +0900

    feat(ui): add barrel file (index.ts)

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,8 @@
+export { default as Badge } from "./badge/Badge"
+export { default as Button } from "./button/Button"
+export { default as Header } from "./header/Header"
+export { default as MonthlyCalendar } from "./calendar/monthlyCalendar/MonthlyCalendar"
+
+// Type exports
+export type { ButtonVariants } from "./button/Button"
+export type { BadgeVariants } from "./badge/Badge.css"
