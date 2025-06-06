32f2b4d091107397b1027c56f3c4883c2531d8b9
d5ng <dong5ffice@gmail.com>
2025-06-07 03:31:17 +0900

    feat(MatchList): implement MatchList and MatchListItem components with styling

---
+++ src/features/matchList/ui/MatchList/MatchListItem.css.ts
@@ -0,0 +1,54 @@
+import { style } from "@vanilla-extract/css"
+import { colorVars, spacing, typography } from "@/shared/tokens"
+
+export const mathItemlayout = style({
+  display: "flex",
+  alignItems: "center",
+  gap: spacing[2.5],
+  padding: spacing[4],
+  borderBottom: `1px solid ${colorVars.neutral.light[200]}`,
+})
+
+export const matchItemInfo = style({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+})
+
+export const matchItemTime = style({
+  ...typography.heading.xl,
+})
+
+export const matchItemTitle = style({
+  ...typography.body.lg,
+  fontWeight: 500,
+})
+
+export const matchItemBadge = style({
+  width: "70px",
+})
+
+export const matchItemTag = style({
+  display: "flex",
+  alignItems: "center",
+  ...typography.body.sm,
+  color: colorVars.neutral.dark[600],
+})
+
+export const matchItemTagItem = style({
+  position: "relative",
+  selectors: {
+    "&:not(:last-child)::after": {
+      content: "•",
+      color: colorVars.neutral.dark[500],
+      padding: "0 3px",
+    },
+  },
+})
+
+export const matchItemRight = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: spacing[1],
+})
