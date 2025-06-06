8fc25314dbcf4052f8f26b32cd6585ca2973b5d5
d5ng <dong5ffice@gmail.com>
2025-06-06 18:25:10 +0900

    feat(assets): add Check icon and update index export

--- src/shared/assets/images/index.ts
+++ src/shared/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as TeamIcon } from "./Team.svg"
 export { default as CalendarCheckIcon } from "./CalendarCheck.svg"
 export { default as AvatarIcon } from "./Avatar.svg"
+export { default as CheckIcon } from "./Check.svg"
