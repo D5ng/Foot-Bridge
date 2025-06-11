1ee509a4e15e44e56379226a407bd5c11455b40e
d5ng <dong5ffice@gmail.com>
2025-06-12 08:56:02 +0900

    feat(index): update index.html for Korean language support, add Kakao SDK, and enhance routing with new page exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
-export { default as MatchListPage } from "./MatchListPage";
+export { default as MatchListPage } from "./MatchListPage"
+export { default as LoginPage } from "./auth/LoginPage"
+export { default as MyPage } from "./MyPage"
+export { default as CreateTeamPage } from "./CreateTeamPage"
