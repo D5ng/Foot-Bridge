ad21dbb4e8b357bb4e05f591293c792a9e97f0f6
d5ng <dong5ffice@gmail.com>
2025-06-23 16:37:43 +0900

    refactor(routes): remove MatchDetailPage route from public routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, type RouteObject } from "react-router"
-import { MatchListPage, LoginPage, MyPage, CreateTeamPage, CreateMatchPage, MatchDetailPage } from "../pages"
+import { MatchListPage, LoginPage, MyPage, CreateTeamPage, CreateMatchPage } from "../pages"
 import ProtectedRoute from "./providers/ProtectedRoute"
 
 /** 인증이 필요한 라우트 */
@@ -27,10 +27,6 @@ const publicRoutes: RouteObject[] = [
   {
     path: "/login",
     Component: LoginPage,
-  },
-  {
-    path: "/match/:matchId",
-    Component: MatchDetailPage,
   },
 ]
 
