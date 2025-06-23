d5df8cfb728d1c679da5787b7ab243a06e3de512
d5ng <dong5ffice@gmail.com>
2025-06-23 17:19:52 +0900

    refactor(index): reorganize exports for better structure and clarity in page components

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,13 @@
-export { default as MatchListPage } from "./MatchListPage/MatchListPage"
+/** Auth */
 export { default as LoginPage } from "./LoginPage/LoginPage"
+
+/** My */
 export { default as MyPage } from "./MyPage/MyPage"
+
+/** Team */
 export { default as CreateTeamPage } from "./CreateTeamPage/CreateTeamPage"
+
+/** Match */
+export { default as MatchListPage } from "./MatchListPage/MatchListPage"
+// export { default as MatchDetailPage } from "./MatchDetailPage/MatchDetailPage"
+export { default as CreateMatchPage } from "./CreateMatchPage/CreateMatchPage"
