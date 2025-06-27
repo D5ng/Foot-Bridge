d1deb0090b93d6c7c6791462279d7c0b5fb066a0
d5ng <dong5ffice@gmail.com>
2025-06-27 17:12:38 +0900

    refactor(CreateTeamPage): update error boundary and loading fallback components to TeamExistsFallback and CreateTeamSkeleton

--- src/pages/CreateTeamPage/CreateTeamPage.tsx
+++ src/pages/CreateTeamPage/CreateTeamPage.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
-import { CreateTeamErrorFallback, CreateTeamGuard } from "@/features/createTeam/ui"
+import { TeamExistsFallback, CreateTeamGuard, CreateTeamSkeleton } from "@/features/createTeam/ui"
 
 export default function CreateTeamPage() {
   return (
-    <ErrorBoundary fallback={<CreateTeamErrorFallback />}>
-      <Suspense fallback={<div>Loading...</div>}>
+    <ErrorBoundary fallback={<TeamExistsFallback />}>
+      <Suspense fallback={<CreateTeamSkeleton />}>
         <CreateTeamGuard />
       </Suspense>
     </ErrorBoundary>
