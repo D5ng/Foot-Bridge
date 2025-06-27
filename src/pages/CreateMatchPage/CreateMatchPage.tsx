a62017caa3043edfaadf5582c814a1878d3b18fe
d5ng <dong5ffice@gmail.com>
2025-06-27 14:52:04 +0900

    fix(CreateMatchPage): update error boundary fallback to use ErrorFallback component with localized error messages

--- src/pages/CreateMatchPage/CreateMatchPage.tsx
+++ src/pages/CreateMatchPage/CreateMatchPage.tsx
@@ -1,10 +1,18 @@
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import { CreateMatchFunnel } from "@/features/createMatch/ui"
+import { ErrorFallback } from "@/shared/ui"
 
 export default function CreateMatchPage() {
   return (
-    <ErrorBoundary fallback={<div>Error</div>}>
+    <ErrorBoundary
+      fallback={
+        <ErrorFallback
+          title="팀 정보를 불러오는데 실패했어요."
+          errorMessage="팀 정보를 불러오는데 실패했어요. 잠시 후 다시 시도해 주시기 바랍니다."
+        />
+      }
+    >
       <Suspense fallback={<div>Loading...</div>}>
         <CreateMatchFunnel />
       </Suspense>
