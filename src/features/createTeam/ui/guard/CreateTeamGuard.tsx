d3f0fefc37cd9ece7a9771e9478d10034f19f5d9
d5ng <dong5ffice@gmail.com>
2025-06-27 14:50:51 +0900

    feat(CreateTeam): implement CreateTeamGuard for team existence validation and error handling

---
+++ src/features/createTeam/ui/guard/CreateTeamGuard.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+import { useTeamByOwnerId } from "@/entities/team"
+import { useAuthStore } from "@/shared/stores/authStore"
+import CreateTeamFunnel from "../form/CreateTeamFunnel"
+
+export default function CreateTeamGuard() {
+  const { user } = useAuthStore()
+  const { data: team } = useTeamByOwnerId(user?.id ?? "")
+
+  if (team) {
+    throw new Error("팀 정보가 이미 존재합니다.")
+  }
+
+  return (
+    <ErrorBoundary fallback={<div>Error</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CreateTeamFunnel />
+      </Suspense>
+    </ErrorBoundary>
+  )
+}
