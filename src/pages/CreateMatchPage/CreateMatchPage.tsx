3901485ba7e3ce835b6d84679e3c4a601024b52c
d5ng <dong5ffice@gmail.com>
2025-06-28 20:03:27 +0900

    refactor(CreateMatchPage): replace CreateMatchFunnel with CreateMatchGuard and update error handling with NoTeamFallback and CreateMatchSkeleton

--- src/pages/CreateMatchPage/CreateMatchPage.tsx
+++ src/pages/CreateMatchPage/CreateMatchPage.tsx
@@ -1,20 +1,12 @@
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
-import { CreateMatchFunnel } from "@/features/createMatch/ui"
-import { ErrorFallback } from "@/shared/ui"
+import { CreateMatchGuard, NoTeamFallback, CreateMatchSkeleton } from "@/features/createMatch/ui"
 
 export default function CreateMatchPage() {
   return (
-    <ErrorBoundary
-      fallback={
-        <ErrorFallback
-          title="팀 정보를 불러오는데 실패했어요."
-          errorMessage="팀 정보를 불러오는데 실패했어요. 잠시 후 다시 시도해 주시기 바랍니다."
-        />
-      }
-    >
-      <Suspense fallback={<div>Loading...</div>}>
-        <CreateMatchFunnel />
+    <ErrorBoundary fallback={<NoTeamFallback />}>
+      <Suspense fallback={<CreateMatchSkeleton />}>
+        <CreateMatchGuard />
       </Suspense>
     </ErrorBoundary>
   )
