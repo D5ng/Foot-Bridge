0fa02dbd7de907e9f560ee4293ee798d0bdbcee5
d5ng <dong5ffice@gmail.com>
2025-07-02 16:47:49 +0900

    feat(DayCell, MonthlyCalendar): enhance date selection and navigation; integrate search parameters for improved date handling

--- src/shared/ui/Calendar/DayCell/DayCell.tsx
+++ src/shared/ui/Calendar/DayCell/DayCell.tsx
@@ -1,4 +1,5 @@
-import { isSameDay } from "date-fns"
+import { format, isSameDay } from "date-fns"
+import { useNavigate } from "react-router"
 import { dayCell, dayNumberStyle, weekdayStyle } from "./DayCell.css"
 import type { DayCellVariantType } from "./DayCell.type"
 import { getDayName, getDayNumber } from "../utils"
@@ -6,18 +7,24 @@ import { getDayName, getDayNumber } from "../utils"
 interface Props {
   date: Date
   selected: boolean
-  // eslint-disable-next-line no-unused-vars
   onSelected: (date: Date) => void
 }
 
 export default function DayCell({ date, selected, onSelected }: Props) {
+  const navigate = useNavigate()
   const dayName = getDayName(date)
   const dayNumber = getDayNumber(date, { isPad: true })
   const isToday = isSameDay(new Date(), date)
+
   const variant: DayCellVariantType = selected ? "selected" : isToday ? "current" : "default"
 
+  const handleSelectedDay = (date: Date) => {
+    onSelected(date)
+    navigate(`?day=${format(date, "dd")}`)
+  }
+
   return (
-    <button className={dayCell({ variant })} onClick={() => onSelected(date)}>
+    <button type="button" className={dayCell({ variant })} onClick={() => handleSelectedDay(date)}>
       <div className={selected ? weekdayStyle.selected : weekdayStyle.unselected}>{dayName}</div>
       <div className={selected ? dayNumberStyle.selected : dayNumberStyle.unselected}>{dayNumber}</div>
     </button>
